a9af3f03482ee08ba86165f6fcae725c7493c908
Sagaa4a <127074133+Bayhaqiindra@users.noreply.github.com>
2025-05-22 02:13:23 +0700

    Tambah widget TitleWithMoreBtn dengan tombol More dan underline

---
+++ lib/screens/home/components/title_with_more_bbtn.dart
@@ -0,0 +1,83 @@
+
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:praktikum8/constants.dart';
+
+class TitleWithMoreBtn extends StatelessWidget {
+  final String title;
+  final VoidCallback press;
+
+  const TitleWithMoreBtn({
+    required this.title,
+    required this.press,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+      child: Row(
+        children: [
+          TitleWithCustomUnderline(text: title),
+          const Spacer(),
+          TextButton(
+            style: TextButton.styleFrom(
+              backgroundColor: kPrimaryColor,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            ),
+            onPressed: press,
+            child: const Text(
+              "More",
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class TitleWithCustomUnderline extends StatelessWidget {
+  final String text;
+
+  const TitleWithCustomUnderline({
+    required this.text,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 24,
+      child: Stack(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: kDefaultPadding / 4),
+            child: Text(
+              text,
+              style: const TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              margin: const EdgeInsets.only(right: kDefaultPadding / 4),
+              height: 7,
+              color: kPrimaryColor.withOpacity(0.2),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
