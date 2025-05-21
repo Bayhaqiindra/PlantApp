a3e506dc67fd8a2a45f75b24e350eaff4f851b42
Sagaa4a <127074133+Bayhaqiindra@users.noreply.github.com>
2025-05-22 01:58:37 +0700

    Tambah layout halaman detail dengan tombol Buy dan Description.

---
+++ lib/screens/details/components/body.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:praktikum8/constants.dart';
+import 'package:praktikum8/screens/details/components/image_and_icons.dart';
+import 'package:praktikum8/screens/details/components/title_and_price.dart';
+
+class Body extends StatelessWidget {
+  const Body({Key? key}) : super(key: key); // Tambahkan const constructor
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          ImageAndIcons(size: size),
+          const TitleAndPrice(title: "Angelica", country: "Russia", price: 440),
+          const SizedBox(height: kDefaultPadding),
+          Row(
+            children: <Widget>[
+              SizedBox(
+                width: size.width / 2,
+                height: 70,
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: kPrimaryColor,
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(20),
+                      ),
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: const Text(
+                    "Buy Now",
+                    style: TextStyle(color: Colors.white, fontSize: 16),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  onPressed: () {},
+                  child: const Text("Description"),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
