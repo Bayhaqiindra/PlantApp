9db0d1c0c193858ddc7ed642c64cdcb0e6993d16
Sagaa4a <127074133+Bayhaqiindra@users.noreply.github.com>
2025-05-22 02:24:32 +0700

    buat halaman peta dengan Google Maps dan lokasi user

---
+++ lib/map_page.dart
@@ -0,0 +1,13 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class MapPage extends StatefulWidget {
+  const MapPage({super.key});
+
+  @override
+  State<MapPage> createState() => _MapPageState();
+}
+
